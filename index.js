ed487f24edb274af14ed62bef05610c5ad346097
oscar ochanda <oscarochanda@gmail.com>
2025-07-06 17:31:25 +0300

    Array-Reduce-Lab

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+//const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+const batteryBatches = [
+  { batch: 1, amount: 5 },
+  { batch: 2, amount: 8 },
+  { batch: 3, amount: 12 },
+  { batch: 4, amount: 6 },
+];
+
+const totalBatteries = batteryBatches.reduce((accumulator, currentBatch) => {
+  return accumulator + currentBatch.amount;
+}, 0); // The '0' is the initial value of the accumulator
